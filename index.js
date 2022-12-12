ed416a978f77dee67b9a0c4a9eb7b2141663dc05
Nel <nel.renaudin@gmail.com>
2022-12-12 14:37:10 +0100

    Create createSquareElem and createShipElem dom methods

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import { Gameboard } from './modules/gameboard.js';
-import { createGameboardElem, displayGameboardElem } from './modules/dom.js';
+import {
+  createShipElem,
+  createGameboardElem,
+  displayGameboardElem,
+} from './modules/dom.js';
+import { Ship } from './modules/ship.js';
 
 const gameboard = new Gameboard();
+const ship = new Ship(3);
+gameboard.placeShip(ship, 4, 2);
+
 const gameboardElem = createGameboardElem(gameboard);
+
 displayGameboardElem(gameboardElem);
--- modules/dom.js
+++ modules/dom.js
@@ -1,13 +1,55 @@
 const pageContent = document.querySelector('#content');
 
+function createSquareElem() {
+  const squareElem = document.createElement('div');
+  squareElem.className = 'square';
+  return squareElem;
+}
+
+export function createShipElem(ship) {
+  const shipElem = document.createElement('div');
+  shipElem.className = 'ship';
+
+  for (let i = 0; i < ship.length; i++) {
+    const squareElem = createSquareElem();
+    shipElem.appendChild(squareElem);
+  }
+
+  if (ship.dir === 'h') {
+    shipElem.style.gridTemplateColumns = `repeat(${ship.length}, var(--square-size))`;
+  } else if (ship.dir === 'v') {
+    shipElem.style.gridTemplateRows = `repeat(${ship.length}, var(--square-size))`;
+  }
+
+  if (ship.x && ship.y) {
+    shipElem.style.top = `calc(var(--square-size) * ${ship.y})`;
+    shipElem.style.left = `calc(var(--square-size) * ${ship.x})`;
+  }
+
+  return shipElem;
+}
+
 export function createGameboardElem(gameboard) {
   const gameboardElem = document.createElement('div');
+  const squaresWrapper = document.createElement('div');
+  const shipsWrapper = document.createElement('div');
   gameboardElem.className = 'gameboard';
+  squaresWrapper.className = 'grid';
+  shipsWrapper.className = 'ships';
+
   for (const square of gameboard.grid) {
-    const squareElem = document.createElement('div');
-    squareElem.className = 'square';
-    gameboardElem.appendChild(squareElem);
+    const squareElem = createSquareElem();
+    squaresWrapper.appendChild(squareElem);
   }
+
+  for (const ship of gameboard.placedShips) {
+    const shipElem = createShipElem(ship);
+    shipsWrapper.appendChild(shipElem);
+  }
+
+  gameboardElem.appendChild(squaresWrapper);
+  gameboardElem.appendChild(shipsWrapper);
+
   return gameboardElem;
 }
 
