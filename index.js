9ed5aabcd69443c1ec3577c098b7865dc157a0e9
Nel <nel.renaudin@gmail.com>
2022-12-12 10:23:07 +0100

    Create displayGameboardElem function

---
+++ index.js
@@ -0,0 +1,6 @@
+import { Gameboard } from './modules/gameboard.js';
+import { createGameboardElem, displayGameboardElem } from './modules/dom.js';
+
+const gameboard = new Gameboard();
+const gameboardElem = createGameboardElem(gameboard);
+displayGameboardElem(gameboardElem);
--- modules/dom.js
+++ modules/dom.js
@@ -1,9 +1,16 @@
+const pageContent = document.querySelector('#content');
+
 export function createGameboardElem(gameboard) {
   const gameboardElem = document.createElement('div');
   gameboardElem.className = 'gameboard';
   for (const square of gameboard.grid) {
     const squareElem = document.createElement('div');
     squareElem.className = 'square';
+    gameboardElem.appendChild(squareElem);
   }
   return gameboardElem;
+}
+
+export function displayGameboardElem(gameboardElem) {
+  pageContent.appendChild(gameboardElem);
 }
