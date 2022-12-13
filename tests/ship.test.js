5585f869c0ef9e5813d5d87045dcf0f1ae0a3f96
Nel <nel.renaudin@gmail.com>
2022-12-13 07:26:14 +0100

    Write test for isPlaced ship property

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,4 +1,5 @@
 import { Ship } from '../modules/ship.js';
+import { Gameboard } from '../modules/gameboard.js';
 
 test('Creating a ship', () => {
   const ship = new Ship(3);
@@ -20,4 +21,12 @@ test('Sunking a ship', () => {
   ship.hit();
   expect(ship.hitsNb).toBe(3);
   expect(ship.isSunk).toBe(true);
+});
+
+test('Placing ship, checking if ship is placed', () => {
+  const ship = new Ship(3);
+  const gameboard = new Gameboard();
+  expect(ship.isPlaced).toBe(false);
+  gameboard.placeShip(ship, 3, 3, 'v');
+  expect(ship.isPlaced).toBe(true);
 });
